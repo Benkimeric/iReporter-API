de79619114766ec6e78b56d697bf6e1f43bb6e84
Benkim <benkimeric@gmail.com>
2018-12-03 20:21:57 +0300

    [Finishes #162387835] update routes.py file

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -1,6 +1,6 @@
 
 from .views.red_flags_view import Records, OneRecord, EditComment,EditLocation
-from ..v1.views.users_view import Users, OneUser,Login
+
 
 from flask_restful import Api,Resource
 from flask import Blueprint
@@ -13,10 +13,3 @@ api.add_resource(Records, 'red-flags')
 api.add_resource(OneRecord, "red-flags/<int:records_id>")
 api.add_resource(EditComment, "red-flags/<int:records_id>/comment")
 api.add_resource(EditLocation, "red-flags/<int:records_id>/location")
-
-#for users
-api.add_resource(Users, 'users')
-api.add_resource(OneUser, 'users/<int:user_id>') 
-
-#login
-api.add_resource(Login, 'users/auth/login')
