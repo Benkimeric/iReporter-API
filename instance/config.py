45e4ccd90cef8c0e4f2d5b18b5501cd42264e90c
Benkim <benkimeric@gmail.com>
2018-12-12 04:41:50 +0300

    [Feature #162411303] add create incident record models and views

--- instance/config.py
+++ instance/config.py
@@ -4,35 +4,25 @@ import os
 class Config(object):
     """Parent configuration class."""
     DEBUG = False
-    CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
 
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
+    DATABASE_URL = "dbname='ireporter' host='localhost' port='5432' user='postgres'\
+     password='kali12'"
 
 
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    DEBUG = True
-
-
-class StagingConfig(Config):
-    """Configurations for Staging."""
-    DEBUG = True
-
-
-class ProductionConfig(Config):
-    """Configurations for Production."""
     DEBUG = False
-    TESTING = False
+    DATABASE_URL = "dbname='test_ireporter' host='localhost' port='5432' user='postgres'\
+     password='kali12'"
 
 
 app_config = {
     'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'staging': StagingConfig,
-    'production': ProductionConfig,
+    'testing': TestingConfig
 }
