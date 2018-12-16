54b04c5b4ca35a04d862bf784ae45c27e71a4c5e
Benkim <benkimeric@gmail.com>
2018-12-16 08:08:55 +0300

    [Chore #162685198] add heroku configurations

--- instance/config.py
+++ instance/config.py
@@ -14,6 +14,14 @@ class DevelopmentConfig(Config):
      password='kali12'"
 
 
+class ProductionConfig(Config):
+    """"Configurations for production"""
+
+    DEBUG = False
+    TESTING = False
+    DATABASE_URL = "postgres://szjgiekrolovoc:a68f2b6ca11a48ac9eb8347eb2d9fad9b378aad66611b09cc728dc56f3ec3b72@ec2-54-247-102-1.eu-west-1.compute.amazonaws.com:5432/dd8qlehec47quf"
+
+
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
@@ -24,5 +32,6 @@ class TestingConfig(Config):
 
 app_config = {
     'development': DevelopmentConfig,
-    'testing': TestingConfig
+    'testing': TestingConfig,
+    'production': ProductionConfig
 }
